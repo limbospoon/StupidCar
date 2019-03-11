b0ad20df71f48839ca1db8d35aa73270b0d3c083
John <magicspoon78@gmail.com>
2019-03-11 23:10:48 +0000

    Added imports to MyAI

--- Self_Driving_Car/myai.py
+++ Self_Driving_Car/myai.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as func
+import torch.optim as opt
+import torch.autograd as ag
+
+from torch.autograd import Variable
