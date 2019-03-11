def656a32f7405a28afe6a225875801210be8ca5
John <magicspoon78@gmail.com>
2019-03-11 23:28:03 +0000

    Added Network class and init function

--- Self_Driving_Car/myai.py
+++ Self_Driving_Car/myai.py
@@ -10,3 +10,15 @@ import torch.optim as opt
 import torch.autograd as ag
 
 from torch.autograd import Variable
+
+#Creating NN architecture
+
+class Network(nn.Module):
+    
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
+
