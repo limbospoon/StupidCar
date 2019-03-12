684c19186d21938b6583eb81c111358b2cc336b9
John <magicspoon78@gmail.com>
2019-03-12 00:33:47 +0000

    Added selection function

--- Self_Driving_Car/myai.py
+++ Self_Driving_Car/myai.py
@@ -59,6 +59,11 @@ class Dqn():
         self.last_action = 0
         self.last_reward = 0
         
+    def select_action(self, state):
+        probs = func.softmax(self.model(Variable(state, volatile = True)) * 7)
+        action = probs.multinomial()
+        return action.data[0,0]
+        
         
         
         
